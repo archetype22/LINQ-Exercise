ba9731e4322eab4b01072bc1b95bf24552e62d61
nathan stephens <nstephens1234567@gmail.com>
2022-12-20 14:00:39 -0600

    initial commit

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -4,7 +4,17 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<string> VideoGames = new List<string>
+            {"Call of Duty Modern Warfare II","God of War Ragnarok","Call of Duty Cold War","God of War","Assassin's Creed Valhalla" };
+            
+            IEnumerable<string> longVideoGames = VideoGames.OrderByDescending(x => x.Length);
+
+            foreach (string item in longVideoGames) 
+            { 
+                Console.WriteLine(item);                
+            }
+                Console.WriteLine();
+                Console.WriteLine(" ^-- All the games a parent no longer has time to play..");
         }
     }
 }
